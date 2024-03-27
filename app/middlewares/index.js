978581b3a1302639703df71fa3a1cb7b23576cdf
Om Solanki <om@Oms-Laptop.local>
2024-03-27 09:39:57 -0400

    Added Code for mail

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -6,11 +6,14 @@ async function CheckDbConnectionAndCacheControlAndPayloadCheck(req, res, next) {
     res.setHeader("Cache-Control", "no-cache");
 
     if (req.method === "GET") {
-        if (req.get("content-length") >= 1 || req._parsedUrl.search != null) {
-            // console.log("Get has payload");
-            logger.warn('Get Method Cannot have PayLoad');
-            res.status(400).end();
-            return;
+        if(req._parsedUrl.query && req._parsedUrl.query.split('=')[0] != 'token' && req._parsedUrl.pathname != '/v1/user/email')
+        {
+            if (req.get("content-length") >= 1 || req._parsedUrl.search != null) {
+                // console.log("Get has payload");
+                logger.warn('Get Method Cannot have PayLoad1');
+                res.status(400).end();
+                return;
+            }
         }
     }
     try {
