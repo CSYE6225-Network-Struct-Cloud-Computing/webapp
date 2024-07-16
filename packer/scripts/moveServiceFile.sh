6ff4d9a49bbcc5ab54188cbc7318d96de44ecb52
Om Solanki <om@Oms-MacBook-Air.local>
2024-07-16 09:34:30 -0400

    centos-version-09 changes

--- packer/scripts/moveServiceFile.sh
+++ packer/scripts/moveServiceFile.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 sudo mv /tmp/runApp.service /etc/systemd/system/runApp.service
+
+# Verify the file has been moved
+if [[ -f /etc/systemd/system/runApp.service ]]; then
+    echo "Service file moved successfully."
+else
+    echo "Failed to move the service file."
+    exit 1
+fi
