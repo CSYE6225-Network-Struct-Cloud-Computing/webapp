81ac18d7aa149c1026fff2c931ba47864c32e41e
Om Solanki <om@Oms-MacBook-Air.local>
2024-07-18 11:22:23 -0400

    centos 9 changes

--- packer/scripts/moveServiceFile.sh
+++ packer/scripts/moveServiceFile.sh
@@ -2,15 +2,3 @@
 
 sudo mv /tmp/runApp.service /etc/systemd/system/runApp.service
 
-# Verify the file has been moved
-if [[ -f /etc/systemd/system/runApp.service ]]; then
-    echo "Service file moved successfully."
-else
-    echo "Failed to move the service file."
-    exit 1
-fi
-
-# # If SELinux is enforcing, set the correct SELinux context
-# if [ "$(getenforce)" = "Enforcing" ]; then
-#   sudo chcon -t systemd_unit_file_t /home/csye6225/app/.env
-# fi
